a370ebdb3f5bd4ecea687be4d40a99edb89957cb
Konstatntin <109268327+konstantin-kif@users.noreply.github.com>
2022-11-08 22:31:05 +0700

    sum

---
+++ les23.py
@@ -0,0 +1,21 @@
+# Cложить два числа
+
+#x = int(input('x = ' ))
+#y = int(input('y = '))
+#print(f'{x} + {y} = {x + y}')
+
+
+#def sum(a, b):
+#    return(a + b)
+    
+#x = int(input('x = ' ))
+#y = int(input('y = '))
+#print(f'{x} + {y} = {sum(x, y)}')
+
+
+#sum = lambda a, b: a + b
+#x = int(input('x = ' ))
+#y = int(input('y = '))
+#print(f'{x} + {y} = {sum(x, y)}')
+
+
